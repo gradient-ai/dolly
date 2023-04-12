2d67fd3e30fec9de09c86c57811815e2cabbca43
jameshskelton <jameshskelton@gmail.com>
2023-04-12 14:47:10 -0500

    added serving app

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8-slim-buster
+
+RUN apt-get update \
+  && apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y git
+COPY cp -a /*/. ./
+RUN pip install -r requirements.txt
+
+CMD python app.py --share
---
+++ app.py
@@ -0,0 +1,25 @@
+import gradio as gr
+
+def infer(inp):
+    return instruct_pipeline(inp)
+
+with gr.Blocks() as main:
+    description_label = """
+    <p style="text-align: center;">
+        <span style="font-size: 28px; font-weight: bold;">Dolly - Inference</span>
+        <br>
+        Databricks’ dolly-v2-12b, an instruction-following large language model trained \n
+        on the Databricks machine learning platform that is licensed for commercial use. \n        
+    </p>
+    <p style="text-align: center;">
+        <br>
+        Enter your text prompt below, and hit `Run inference` to generate text.   
+    </p>
+    """
+    gr.HTML(description_label)
+    inp = gr.Textbox(label = 'Text input')
+    out = gr.Textbox(label = 'Text output')
+    with gr.Row():
+        but = gr.Button('Run inference')
+    but.click(fn = infer, inputs = inp, outputs = out)
+main.launch(share = True)
